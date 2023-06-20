37284e8faaad5b6c16bc1c4e2250d3128915c9d7
Sergey Khlebnikov <136861017+sergk-dev@users.noreply.github.com>
2023-06-20 17:49:07 +0000

    Сделал раздел молитв, отладил ввод текста через веб ап.

--- all12steps/all12steps/settings.py
+++ all12steps/all12steps/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from os import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-xxbs$uf_qvs4u#1qo7g_ix$g7cz28yyz)60oi4=_6@rs7sa4vx"
+SECRET_KEY = environ['DJANGO_SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "bender"
 ]
 
 MIDDLEWARE = [
